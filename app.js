87e928bd430e3089a5c6ecd5d637001717e74c76
Hector Milla <37377168+HectorMilla@users.noreply.github.com>
2023-10-08 14:08:36 -0400

    created function to prompt user for game choice then calculate computer choice and compare the two to pick a winner

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("hi");
+// get player choice
+let playerChoice = () => {
+  let answer = prompt(
+    "What will you choose?(Rock, Paper, or Scissors)"
+  ).toLowerCase();
+  return answer;
+};
+// get computer choice
+let computerChoice = () => {
+  let choices = ["paper", "rock", "scissors"];
+  let choice = choices[Math.floor(Math.random() * choices.length)];
+  return choice;
+};
+
+// pick winner
+let winner = (playerChoice, computerChoice) => {
+  if (playerChoice == computerChoice) {
+    alert("Its a draw");
+  } else if (playerChoice == "paper") {
+    computerChoice == "rock" ? alert("You Win!") : alert("You Lose :(");
+  } else if (playerChoice == "rock") {
+    computerChoice == "scissors" ? alert("You Win!") : alert("You Lose :(");
+  } else if (playerChoice == "scissors") {
+    computerChoice == "paper" ? alert("You Win!") : alert("You Lose :(");
+  }
+};
+
+function game() {
+  let running = true;
+
+  while (running) {
+    let playAgain = prompt("Do you want to play again?(yes/no)").toLowerCase();
+    if (playAgain == "yes") {
+      winner(playerChoice(), computerChoice());
+    } else {
+      running = false;
+      alert("Thank you for playing");
+    }
+  }
+}
+
+game();
