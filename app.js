0c5ab7f6d3ecb0aa47381586eeef8c4ab5e757a8
Hector Milla <37377168+HectorMilla@users.noreply.github.com>
2023-10-08 15:11:40 -0400

    added win condition to equal first player/computer to get to 5 wins will win the whole game

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// get player choice
+//get player choice
 let playerChoice = () => {
   let answer = prompt(
     "What will you choose?(Rock, Paper, or Scissors)"
   ).toLowerCase();
   return answer;
 };
-// get computer choice
+//get computer choice
 let computerChoice = () => {
   let choices = ["paper", "rock", "scissors"];
   let choice = choices[Math.floor(Math.random() * choices.length)];
@@ -13,20 +13,33 @@ let computerChoice = () => {
 };
 
 // pick winner
-let winner = (playerChoice, computerChoice) => {
-  if (playerChoice == computerChoice) {
-    alert("Its a draw");
-  } else if (playerChoice == "paper") {
-    computerChoice == "rock" ? alert("You Win!") : alert("You Lose :(");
-  } else if (playerChoice == "rock") {
-    computerChoice == "scissors" ? alert("You Win!") : alert("You Lose :(");
-  } else if (playerChoice == "scissors") {
-    computerChoice == "paper" ? alert("You Win!") : alert("You Lose :(");
+let winner = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let winningScore = 5;
+  while (playerScore < winningScore && computerScore < winningScore) {
+    let player = playerChoice();
+    let computer = computerChoice();
+    if (player == computer) {
+      alert("its a draw");
+    } else if (player == "paper") {
+      computer == "rock" ? playerScore++ : computerScore++;
+    } else if (player == "rock") {
+      computer == "scissors" ? playerScore++ : computerScore++;
+    } else if (player == "scissors") {
+      computer == "paper" ? playerScore++ : computerScore++;
+    }
+    alert(`The score is Player:${playerScore} Computer:${computerScore}`);
+  }
+  if (playerScore == winningScore) {
+    console.log("you won");
+  } else if (computerScore == winningScore) {
+    console.log("Computer wins! Better luck next time!");
   }
 };
 
 function game() {
-  winner(playerChoice(), computerChoice());
+  winner();
   let playAgain = prompt("Do you want to play again?(yes/no)");
   let running = true;
   while (running) {
